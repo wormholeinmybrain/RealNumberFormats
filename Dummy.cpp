d21473ede15723dae1dc019edb038cab56f90f8e
wormholeinmybrain <95233162+wormholeinmybrain@users.noreply.github.com>
2022-04-22 07:50:44 +0200

    Finish all functions

--- Dummy.cpp
+++ Dummy.cpp
@@ -1,3 +1,5 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "modernize-use-auto"
 // DummyClass.cpp : Defines the entry point for the application.
 //
 #include <cstdio>
@@ -860,163 +862,253 @@ Dummy Dummy::operator/=(Dummy d) {
 #endif
     return Dummy(content);
 }
-/*
-#ifdef VARIABLE_FLOATING_POINT_FLOATX
-//091.5-1
-Dummy Dummy::operator=(Dummy& dm){
-    content = dm.content;
-#ifndef DISABLE_OPERATION_STATISTICS
-    recordExponent(content);
-#endif
-    return *this;
-}
 
-//091.5-2
-Dummy Dummy::operator=(const double d){
-    content = VFP(d);
-#ifndef DISABLE_OPERATION_STATISTICS
-    recordExponent(content);
-#endif
-    return *this;
-}
-
-
-//091.5-3
-Dummy Dummy::operator=(const float f){
-    content = VFP(f);
-#ifndef DISABLE_OPERATION_STATISTICS
-    recordExponent(content);
-#endif
-    return *this;
-}
-
-#endif
-*/
 //092
-DOUBLE fabs(DOUBLE a){
-    return (DOUBLE)((double)fabs((double)a));
-}
-
-DOUBLE trunc(DOUBLE a){
-    return (DOUBLE)((double)trunc((double)a));
-}
-
-DOUBLE atan2(DOUBLE a,DOUBLE b){
-    return (DOUBLE)((double)atan2((double)a,(double)b));
-}
-
-DOUBLE floor(DOUBLE a){
-    return (DOUBLE)((double)floor((double)a));
-}
-
-DOUBLE log10(DOUBLE a){
-    return (DOUBLE)((double)log10((double)a));
-}
-
-DOUBLE hypot(DOUBLE a,DOUBLE b){
-    return (DOUBLE)((double)hypot((double)a,(double)b));
-}
-
-DOUBLE log(DOUBLE a){
-    return (DOUBLE)((double)log((double)a));
-}
-
-DOUBLE exp(DOUBLE a){
-    return (DOUBLE)((double)exp((double)a));
-}
-
-DOUBLE cos(DOUBLE a){
-    return (DOUBLE)((double)cos((double)a));
-}
-
-DOUBLE sin(DOUBLE a){
-    return (DOUBLE)((double)sin((double)a));
-}
-
-DOUBLE cosh(DOUBLE a){
-    return (DOUBLE)((double)cosh((double)a));
-}
-
-DOUBLE sinh(DOUBLE a){
-    return (DOUBLE)((double)sinh((double)a));
-}
-
-DOUBLE tan(DOUBLE a){
-    return (DOUBLE)((double)tan((double)a));
-}
-
-DOUBLE tanh(DOUBLE a){
-    return (DOUBLE)((double)tanh((double)a));
-}
-
-DOUBLE atan(DOUBLE a){
-    return (DOUBLE)((double)atan((double)a));
-}
-
-DOUBLE ceil(DOUBLE a){
-    return (DOUBLE)((double)ceil((double)a));
-}
-
-DOUBLE nearbyint(DOUBLE a){
-    return (DOUBLE)((double)nearbyint((double)a));
-}
-
-DOUBLE pow(DOUBLE a,DOUBLE b){
-    return (DOUBLE)((double)pow((double)a,(double)b));
-}
-
-DOUBLE pow(double a,DOUBLE b){
-    return (DOUBLE)((double)pow(a,(double)b));
-}
-
-DOUBLE pow(DOUBLE a,double b){
-    return (DOUBLE)((double)pow((double)a,b));
-}
-DOUBLE sqrt(DOUBLE a){
-    return (DOUBLE)((double)sqrt((double)a));
-}
-
-DOUBLE isnan(DOUBLE a){
-    return (DOUBLE)((double)isnan((double)a));
-}
-
-DOUBLE logb(DOUBLE a){
-    return (DOUBLE)((double)logb((double)a));
-}
-
-DOUBLE scalbn(DOUBLE a,int i){
-    return (DOUBLE)((double)scalbn((double)a,i));
-}
-
-DOUBLE asin(DOUBLE a){
-    return (DOUBLE)((double)asin((double)a));
-}
-
 DOUBLE acos(DOUBLE a){
-    return (DOUBLE)((double)acos((double)a));
+    double d = (double)acos((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif //end of DISABLE_OPERATION_STATISTICS
+    return (DOUBLE)d;
 }
 
-DOUBLE atanh(DOUBLE a){
-    return (DOUBLE)((double)atanh((double)a));
-}
-
+//093
 DOUBLE acosh(DOUBLE a){
-    return (DOUBLE)((double)acosh((double)a));
+    double d = (double)acosh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
 }
 
+//094
+DOUBLE asin(DOUBLE a){
+    double d = (double)asin((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//095
 DOUBLE asinh(DOUBLE a){
-    return (DOUBLE)((double)asinh((double)a));
+    double d = (double)asinh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
 }
 
-DOUBLE round(DOUBLE a){
-    return (DOUBLE)((double)round((double)a));
+//096
+DOUBLE atan(DOUBLE a){
+    double d = (double)atan((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
 }
 
+//097
+DOUBLE atan2(DOUBLE a,DOUBLE b){
+    double d = (double)atan2((double)a,(double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//098
+DOUBLE atanh(DOUBLE a){
+    double d = (double)atanh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//099
+DOUBLE cos(DOUBLE a){
+    double d = (double)cos((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//100
+DOUBLE cosh(DOUBLE a){
+    double d = (double)cosh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//101
+DOUBLE exp(DOUBLE a){
+    double d = (double)exp((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//102
+DOUBLE hypot(DOUBLE a,double b){
+    double d = (double)hypot((double)a,(double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//103
+DOUBLE log(DOUBLE a){
+    double d = (double)log((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//104
+DOUBLE logb(DOUBLE a){
+    double d = (double)logb((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//105
+DOUBLE log10(DOUBLE a){
+    double d = (double)log10((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//106
+DOUBLE pow(DOUBLE a, DOUBLE b){
+    double d = (double)pow((double)a,(double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//107
+DOUBLE pow(double a, DOUBLE b){
+    double d = (double)pow(a, (double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//108
+DOUBLE pow( DOUBLE a,double b){
+    double d = (double)pow((double)a, b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//109
+DOUBLE sin(DOUBLE a){
+    double d = (double)sin((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//110
+DOUBLE sinh(DOUBLE a){
+    double d = (double)sinh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//111
+DOUBLE sqrt(DOUBLE a){
+    double d = (double)sqrt((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//112
+DOUBLE tan(DOUBLE a){
+    double d = (double)tan((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//113
+DOUBLE tanh(DOUBLE a){
+    double d = (double)tanh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//114
 DOUBLE abs(DOUBLE a){
     return (DOUBLE)((double)abs((double)a));
 }
 
+//115
+DOUBLE ceil(DOUBLE a){
+    return (DOUBLE)((double)ceil((double)a));
+}
 
+//116
+DOUBLE fabs(DOUBLE a){
+    return (DOUBLE)((double)fabs((double)a));
+}
+
+//117
+DOUBLE floor(DOUBLE a){
+    return (DOUBLE)((double)floor((double)a));
+}
+
+//118
+DOUBLE frexp(DOUBLE r, int* exp) {
+    return (DOUBLE)((double)frexp((double)r, exp));
+}
+
+//119
 DOUBLE modf(DOUBLE para, DOUBLE* intpart) {
     double m = 0.0;
     double* mPtr = &m;
@@ -1025,6 +1117,7 @@ DOUBLE modf(DOUBLE para, DOUBLE* intpart) {
     return a;
 }
 
+//120
 DOUBLE modf(double para, DOUBLE* intpart) {
     double m = 0.0;
     double* mPtr = &m;
@@ -1032,13 +1125,36 @@ DOUBLE modf(double para, DOUBLE* intpart) {
     intpart->setContent(m);
     return a;
 }
-//====== The functions mentioned in to-do, tested =======
-DOUBLE frexp(DOUBLE r, int* exp) {
-    return (DOUBLE)((double)frexp((double)r, exp));
+
+//121
+DOUBLE nearbyint(DOUBLE a){
+    return (DOUBLE)((double)nearbyint((double)a));
 }
+
+//122
+DOUBLE round(DOUBLE a){
+    return (DOUBLE)((double)round((double)a));
+}
+
+//123
+DOUBLE scalbn(DOUBLE a,int i){
+    return (DOUBLE)((double)scalbn((double)a,i));
+}
+
+//124
+DOUBLE trunc(DOUBLE a){
+    return (DOUBLE)((double)trunc((double)a));
+}
+
+//====== The functions mentioned in to-do, tested =======
 
 int fpclassify(DOUBLE d) {
     return fpclassify((double)d);
+}
+
+
+DOUBLE isnan(DOUBLE a){
+    return (DOUBLE)((double)isnan((double)a));
 }
 
 bool isfinite(DOUBLE d) {
@@ -1770,10 +1886,10 @@ bool operator<(Dummy d, int i) {
     else
         return false;
 }
-//====//
-//088
+
+//088-f
 Dummy Dummy::operator+=(Dummy d) {
-    content = content + d.content;
+    content = fixedpt_add(content, d.content);
 #ifndef DISABLE_OPERATION_STATISTICS
     incrNumAdd();
     recordExponent(content);
@@ -1781,9 +1897,9 @@ Dummy Dummy::operator+=(Dummy d) {
     return {content};
 }
 
-//089
+//089-f
 Dummy Dummy::operator-=(Dummy d) {
-    content = content - d.content;
+    content = fixedpt_sub(content, d.content);
 #ifndef DISABLE_OPERATION_STATISTICS
     incrNumAdd();
     recordExponent(content);
@@ -1791,9 +1907,9 @@ Dummy Dummy::operator-=(Dummy d) {
     return {content};
 }
 
-//090
+//090-f
 Dummy Dummy::operator*=(Dummy d) {
-    content = content * d.content;
+    content = fixedpt_mul(content,  d.content);
 #ifndef DISABLE_OPERATION_STATISTICS
     incrNumMul();
     recordExponent(content);
@@ -1801,9 +1917,9 @@ Dummy Dummy::operator*=(Dummy d) {
     return {content};
 }
 
-//091
+//091-f
 Dummy Dummy::operator/=(Dummy d) {
-    content = content / d.content;
+    content = fixedpt_div(content, d.content);
 #ifndef DISABLE_OPERATION_STATISTICS
     incrNumDiv();
     recordExponent(content);
@@ -1812,6 +1928,404 @@ Dummy Dummy::operator/=(Dummy d) {
 }
 
 
+/**
+ * Mathematical functions
+ */
+//092-f
+DOUBLE acos(DOUBLE a){
+    double d = (double)acos((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif //end of DISABLE_OPERATION_STATISTICS
+    return (DOUBLE)d;
+}
+
+//093-f
+DOUBLE acosh(DOUBLE a){
+    double d = (double)acosh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//094-f
+DOUBLE asin(DOUBLE a){
+    double d = (double)asin((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//095-f
+DOUBLE asinh(DOUBLE a){
+    double d = (double)asinh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//096-f
+DOUBLE atan(DOUBLE a){
+    double d = (double)atan((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//097-f
+DOUBLE atan2(DOUBLE a,DOUBLE b){
+    double d = (double)atan2((double)a,(double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//098-f
+DOUBLE atanh(DOUBLE a){
+    double d = (double)atanh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//099-f
+DOUBLE cos(DOUBLE a){
+    double d = (double)cos((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//100-f
+DOUBLE cosh(DOUBLE a){
+    double d = (double)cosh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//101-f
+DOUBLE erfc(DOUBLE a) {
+    double d = (double)erfc((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//102-f
+DOUBLE exp(DOUBLE a){
+    double d = (double)exp((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//103-f
+DOUBLE hypot(DOUBLE a,double b){
+    double d = (double)hypot((double)a,(double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//104-f
+DOUBLE log(DOUBLE a){
+    double d = (double)log((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//105-f
+DOUBLE logb(DOUBLE a){
+    double d = (double)logb((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//106-f
+DOUBLE log10(DOUBLE a){
+    double d = (double)log10((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//107-f
+DOUBLE pow(DOUBLE a, DOUBLE b){
+    double d = (double)pow((double)a,(double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//108-f
+DOUBLE pow(double a, DOUBLE b){
+    double d = (double)pow(a, (double)b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//109-f
+DOUBLE pow( DOUBLE a,double b){
+    double d = (double)pow((double)a, b);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//110-f
+DOUBLE sin(DOUBLE a){
+    double d = (double)sin((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//111-f
+DOUBLE sinh(DOUBLE a){
+    double d = (double)sinh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//112-f
+DOUBLE sqrt(DOUBLE a){
+    double d = (double)sqrt((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//113-f
+DOUBLE tan(DOUBLE a){
+    double d = (double)tan((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+//114-f
+DOUBLE tanh(DOUBLE a){
+    double d = (double)tanh((double)a);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::incrNumElem();
+    Dummy::recordExponent(d);
+#endif
+    return (DOUBLE)d;
+}
+
+/**
+ * Other Functions
+ */
+//115-f
+DOUBLE abs(DOUBLE a) {
+    return {fixedpt_abs(a.getContent())};
+}
+
+//116-f
+DOUBLE ceil(DOUBLE a){
+    fixedpt fa = (a.getContent()>0)
+                  ? (a.getContent()&(~FIXEDPT_FMASK)) + fixedpt_fromint(1)
+                  : (a.getContent()&(~FIXEDPT_FMASK));
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::recordExponent(fa);
+#endif
+    return {fa};
+}
+
+//117-f
+/**
+ * Because fabs() can't be overloaded with integer as input, the input
+ * must be converted to double
+ */
+DOUBLE fabs(DOUBLE a){
+    return (DOUBLE)((double)fabs((double)a));
+}
+
+//118-f
+DOUBLE floor(DOUBLE a){
+    fixedpt fa = (a.getContent()>0)
+                 ? (a.getContent()&(~FIXEDPT_FMASK))
+                 : (a.getContent()&(~FIXEDPT_FMASK)) - fixedpt_fromint(1);
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::recordExponent(fa);
+#endif
+    return {fa};
+}
+
+//119-f
+DOUBLE frexp(DOUBLE r, int* exp) {
+    return (DOUBLE)frexp(fixedpt_tofloat(r.getContent()),exp);
+}
+
+//120-f
+DOUBLE modf(DOUBLE d1 , DOUBLE* intpart){
+    fixedpt input = d1.getContent();
+    fixedpt fragpart = input&FIXEDPT_FMASK;
+    *intpart = input&(~(FIXEDPT_FMASK));
+    return {fragpart};
+}
+
+//121-f
+DOUBLE modf(double d, DOUBLE* intpart){
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::recordExponent(d);
+#endif
+    fixedpt input = double2fixed(d);
+    fixedpt fragpart = input&FIXEDPT_FMASK;
+    *intpart = input&(~(FIXEDPT_FMASK));
+    return {fragpart};
+}
+
+//122-f
+DOUBLE nearbyint(DOUBLE a){
+#if defined(FE_DOWNWARD)
+    fixedpt fa = a > 0
+                 ? (a.getContent()&(~FIXEDPT_FMASK))
+                 : (a.getContent()&(~FIXEDPT_FMASK)) - fixedpt_fromint(1);
+#elif defined(FE_TOWARDZERO)
+    fixedpt fa = a.getContent()&(~FIXEDPT_FMASK);
+#elif defined(FE_UPWARD)
+    fixedpt fa = a > 0
+                 ? (a.getContent()&(~FIXEDPT_FMASK)) + fixedpt_fromint(1)
+                 : (a.getContent()&(~FIXEDPT_FMASK));
+#elif defined(FE_TONEAREST)
+    fixedpt fa = a.getContent();
+    if(fa > 0){
+        if((fa&(fixedpt(1)<<(FIXEDPT_FBITS-1)))!=0)
+            fa = (fa & (~FIXEDPT_FMASK)) + ((fixedpt)1<<FIXEDPT_FBITS);
+        else
+            fa = fa & (~FIXEDPT_FMASK);
+    }
+    else{
+        if((fa&(fixedpt(1)<<(FIXEDPT_FBITS-1)))!=0)
+            fa = (fa & (~FIXEDPT_FMASK)) -  ((fixedpt)1<<FIXEDPT_FBITS);
+        else
+            fa = fa & (~FIXEDPT_FMASK);
+    }
+#else
+    fixedpt fa = a.getContent();
+    if(fa > 0){
+        if((fa&(fixedpt(1)<<(FIXEDPT_FBITS-1)))!=0)
+            fa = (fa & (~FIXEDPT_FMASK)) + fixedpt_fromint(1);
+        else
+            fa = fa & (~FIXEDPT_FMASK);
+    }
+    else{
+        if((fa&(fixedpt(1)<<(FIXEDPT_FBITS-1)))!=0)
+            fa = (fa & (~FIXEDPT_FMASK)) -  fixedpt_fromint(1);
+        else
+            fa = fa & (~FIXEDPT_FMASK);
+    }
+#endif //end of FE_ macros
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::recordExponent(fa);
+#endif
+    return {fa};
+}
+
+//123-f
+DOUBLE round(DOUBLE a){
+    fixedpt fa = a.getContent();
+    if(fa > 0){
+        if((fa&(fixedpt(1)<<(FIXEDPT_FBITS-1)))!=0)
+            fa = (fa & (~FIXEDPT_FMASK)) + fixedpt_fromint(1);
+        else
+            fa = fa & (~FIXEDPT_FMASK);
+    }
+    else{
+        if((fa&(fixedpt(1)<<(FIXEDPT_FBITS-1)))!=0)
+            fa = (fa & (~FIXEDPT_FMASK)) - fixedpt_fracpart(1);
+        else
+            fa = fa & (~FIXEDPT_FMASK);
+    }
+#ifndef DISABLE_OPERATION_STATISTICS
+    Dummy::recordExponent(fa);
+#endif
+    return {fa};
+}
+
+//124-f (?)
+DOUBLE scalbn(DOUBLE a ,int i){
+    return (a.getContent() << i);
+}
+
+//125-f
+DOUBLE trunc(DOUBLE a){
+    return {a.getContent()&(~FIXEDPT_FMASK)};
+}
+
+//126-f
+int fpclassify(DOUBLE d){
+    return fpclassify((double)d);
+}
+
+//127-f
+DOUBLE isnan(DOUBLE d){
+    return fixedpt_fromint(1);
+}
+
+//128-f
+bool isfinite(DOUBLE d){
+    return isfinite((double)d);
+}
+
+//129-f
+bool isnormal(DOUBLE d){
+    return isnormal((double)d);
+}
+
+//130-f
+bool isinf(DOUBLE d) {
+    return isinf((double)d);
+}
 
 
 
@@ -1823,3 +2337,5 @@ Dummy Dummy::operator/=(Dummy d) {
 
 
 
+
+#pragma clang diagnostic pop
